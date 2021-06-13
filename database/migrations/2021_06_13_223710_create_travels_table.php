372a5e6dc7a6f54dc2fa8e6458e7694ae8a59367
karin valcanover <valcanoverkarin@outlook.it>
2021-06-14 01:19:44 +0200

    first commit

---
+++ database/migrations/2021_06_13_223710_create_travels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('destinaton', 100);
+            $table->tinyInteger('days');
+            $table->smallInteger('duration');
+            $table->float('price', 7, 2);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
